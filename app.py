19a7984d0341ca5448851fb5e36e9277d48d32dd
S-Hacker-I <92311456+S-Hacker-I@users.noreply.github.com>
2024-09-13 07:56:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ if __name__ == '__main__':
     # Start the balance reset scheduler
     Timer(0, reset_balance).start()  # Start immediately
     # Set custom port and enable threading
-    app.run(debug=True, host='0.0.0.0', port=5001, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
